f8f52a2d51f8565d855b0edf3bee390191085c5c
saikumark <kolichelimisaikumar9@gmail.com>
2022-08-18 07:31:35 +0530

    and

---
+++ and.js
@@ -0,0 +1,18 @@
+const a = true, b = false;
+const c = 4;
+
+// logical AND
+console.log(a && a); // true
+console.log(a && b);  // false
+
+console.log((c > 2) && (c < 2)); // false
+
+
+
+var num=10;
+var num1=20;
+if (num>num1 && num<num1){
+console.log(true)}
+else {
+    console.log(false)
+}
